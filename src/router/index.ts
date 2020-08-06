0bfb5af85c45809ff0b87a01fc91c5380ee804e4
v_tianguoping <v_tianguoping@100tal.com>
2020-08-06 20:02:19 +0800

    first commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,19 @@
-import Vue from "vue";
-import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
-
-Vue.use(VueRouter);
+import { RouteConfig, createRouter, createWebHistory } from "vue-router";
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
+    name: "name",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "demo" */ "../views/demo.vue")
   }
 ];
 
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
   routes
 });
 
---
+++ src/views/demo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{ countRef }}</div>
+</template>
+<script>
+import { ref, reactive } from "vue";
+export default {
+  setup () {
+    const countRef = ref(0);
+    return {
+      countRef
+    }
+  }
+};
+</script>
